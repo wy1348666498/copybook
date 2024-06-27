d3e49ed6f43cc8d7050c91ad5ce68a1499563523
Wei Yang <2116466260@qq.com>
2024-06-27 17:30:19 +0800

    feat: 确定导出pdf方案

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,59 @@
 <template>
-  <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
-  </view>
+  <div class="container">
+    <input v-model="text" placeholder="输入练习内容" />
+    <button @click="generatePDF">生成 PDF</button>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
-const title = ref('Hello')
+import { PDFDocument } from 'pdf-lib'
+
+const text = ref('')
+
+const generatePDF = async () => {
+  // 创建 PDF 文档
+  const pdfDoc = await PDFDocument.create()
+  const page = pdfDoc.addPage([595.28, 841.89]) // A4 size
+
+  const fs = uni.getFileSystemManager()
+
+  uni.chooseMedia({
+    count: 1,
+    mediaType: ['image'],
+    sourceType: ['album', 'camera'],
+    success: async (res) => {
+      const sBase64 = fs.readFileSync(res.tempFiles[0].tempFilePath, 'base64')
+      const jpgImage = await pdfDoc.embedPng(sBase64)
+      page.drawImage(jpgImage, {
+        x: 20,
+        y: 20,
+        width: page.getWidth() - 40,
+        height: page.getHeight() - 40,
+      })
+
+      // 保存 PDF 文档
+      const pdfBytes = await pdfDoc.saveAsBase64()
+      const filePath = uni.env.USER_DATA_PATH + '/' + 'pdf.pdf'
+      fs.writeFile({
+        data: pdfBytes,
+        filePath,
+        encoding: 'base64',
+        success: (r) => {
+          uni.openDocument({
+            filePath,
+          })
+        },
+        complete: (r) => {
+          console.log(r)
+        },
+      })
+    },
+  })
+}
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin: 200rpx auto 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+<style scoped>
+.container {
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite'
 import Uni from '@dcloudio/vite-plugin-uni'
 import ViteRestart from 'vite-plugin-restart'
 import UnoCSS from 'unocss/vite'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
+    resolve: {
+      // 设置别名
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
     esbuild: {
       // 判断是否为生产环境
       drop: process.env.NODE_ENV === 'production' ? ['console', 'debugger'] : [],
